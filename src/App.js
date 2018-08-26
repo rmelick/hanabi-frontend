32ef82b27dcab13aea57f1f345c820071b31c967
Russell Melick <russell.melick@kiwigrid.com>
2018-08-26 14:54:22 -0700

    colors and board

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import example_state from "./example_state";
+console.log("example_state");
+console.log(example_state);
 
 
 class App extends Component {
@@ -26,35 +29,51 @@ class App extends Component {
 }
 
 class Tile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.rank = props.tile.rank;
+    this.color = props.tile.color;
+  }
+
   render() {
     return (
-      <button className="tile">
-        {/* TODO */}
+      <button className={`tile color-${this.color}`}>
+        {this.rank}
       </button>
     );
   }
 }
 
 class PlayerHand extends React.Component {
-  renderTile(i) {
-    return <Tile />
+  constructor(props) {
+    super(props);
+    this.tiles = props.player.tiles;
+  }
+
+  renderTile(tile) {
+    return <Tile tile={tile}/>
   }
 
   render() {
     return (
       <div className="player-hand">
-        {this.renderTile(0)}
-        {this.renderTile(1)}
-        {this.renderTile(2)}
-        {this.renderTile(3)}
+        {this.renderTile(this.tiles[0])}
+        {this.renderTile(this.tiles[1])}
+        {this.renderTile(this.tiles[2])}
+        {this.renderTile(this.tiles[3])}
       </div>
     );
   }
 }
 
 class Board extends React.Component {
-  renderPlayerHand(i) {
-    return <PlayerHand />;
+  constructor(props) {
+    super(props);
+    this.players = props.game_state.players;
+  }
+
+  renderPlayerHand(player) {
+    return <PlayerHand player={player}/>;
   }
 
   render() {
@@ -63,21 +82,26 @@ class Board extends React.Component {
     return (
       <div>
         <div className="status">{status}</div>
-        {this.renderPlayerHand(0)}
-        {this.renderPlayerHand(1)}
-        {this.renderPlayerHand(2)}
-        {this.renderPlayerHand(3)}
+        {this.renderPlayerHand(this.players[0])}
+        {this.renderPlayerHand(this.players[1])}
+        {this.renderPlayerHand(this.players[2])}
+        {this.renderPlayerHand(this.players[3])}
       </div>
     );
   }
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = example_state;
+  }
+
   render() {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board game_state={this.state}/>
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
