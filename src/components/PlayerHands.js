d11b19fed7808d5b40011168b3a372eda6a55338
Russell Melick <russell.melick@kiwigrid.com>
2018-09-09 11:56:08 -0700

    Fix CurrentPlayer calculation

--- src/components/PlayerHands.js
+++ src/components/PlayerHands.js
@@ -10,8 +10,21 @@ function renderThisPlayerHand(player) {
   return <ThisPlayerHand player={player} key={player.id}/>;
 }
 
+function findCurrentPlayerName(players) {
+  if (players.this_player.is_current_player) {
+    return players.this_player.name
+  } else {
+    let player;
+    for (player in players.other_players) {
+      if (player.is_current_player) {
+        return player.name
+      }
+    }
+  }
+}
+
 export function PlayerHands(props) {
-  const status = 'Next player: X';
+  const status = 'Current player: ' + findCurrentPlayerName(props.players);
   const thisPlayer = renderThisPlayerHand(props.players.this_player);
   const otherPlayers = props.players.other_players.map(player => renderPlayerHand(player, props.hints[player.id]));
 
